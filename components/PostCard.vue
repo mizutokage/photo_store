20045c39af197325cba65d6fe6bd3aa500929289
mizutokage <mztmztphy@i.softbank.jp>
2022-07-14 19:09:30 +0900

    first commit

---
+++ components/PostCard.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h2>{{ post.name }}</h2>
+        <p>{{ post.age }}</p>
+        <a href="">
+            <v-img
+                class="white--text"
+                height="170px"
+                width="170px"
+                v-bind:src="post.thumbnail.url"
+            ></v-img>
+        </a>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "PostCard",
+    props: ["post"]
+}
+</script>
---
+++ pages/PostList.vue
@@ -0,0 +1,49 @@
+<template>
+    <div>
+        <post-card
+            v-for="post in posts"
+            v-bind:key="post.id"
+            :post=post
+        ></post-card>
+    </div>
+</template>
+
+<script>
+import PostCard from '../components/PostCard.vue'
+
+export default {
+    components: {
+        PostCard,
+    },
+    data() {
+        return {
+            posts: [
+                {
+                    id: 1,
+                    name: "mizuto", 
+                    age: "25",
+                    thumbnail: {
+                        url: "https://picsum.photos/800/400?image=80"
+                    }
+                },
+                {
+                    id: 2, 
+                    name: "nanako", 
+                    age: "23",
+                    thumbnail: {
+                        url: "https://picsum.photos/800/400?image=90"
+                    }
+                },
+                {
+                    id: 3, 
+                    name: "mizuki", 
+                    age: "19",
+                    thumbnail: {
+                        url: "https://picsum.photos/800/400?image=100"
+                    }
+                }
+            ]
+        }
+    },
+}
+</script>
